53673c3f73a8b44d6810d7c5f61125f328fe6556
jwyc <janis.wy.chan@gmail.com>
2021-12-21 22:25:03 -0800

    replaced forEach with for-of, refactored wordInprogress.innerText without use of additional var.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const wordInProgress = document.querySelector(".word-in-progress");
 // <p> remaining guesses display
 const remainGuesses = document.querySelector(".remaining");
 //<span> inside <p> where remaining guesses display
-const  numRemainGuesses = document.querySelector(".remaining span");
+const  remainGuessesSpan = document.querySelector(".remaining span");
 //response message to players guess
 const response = document.querySelector(".message");
 //play again button - hidden -
@@ -19,20 +19,37 @@ const word = "magnolia";
 
 
 
-const placeHolder = function(){
-   //-/console.log(word.split(""));
-   const wordArray = word.split("");
+const placeHolder = function(word){
+//test// console.log(word.split(""));
+   //-//const wordArray = word.split("");
    const revealLetters = []
-   //-/console.log(revealLetters);
-   //-/console.log(wordArray.length);
+//test// console.log(revealLetters);
+//test// console.log(wordArray.length);
+   
+//*-*// a forEach is overkill, a "for in" looks at array values only, no need for index in this function //
+   //-//wordArray.forEach (function(letter, index){
+     //-// revealLetters.push("●");
+      //test//console.log(`${letter} ${index}`)
+      //-// } 
+   //-// );
 
-   wordArray.forEach (function(letter, index){
+   for (const letter of word){
+      console.log(letter);
       revealLetters.push("●");
-      //-/console.log(`${letter} ${index}`)
-   });
-  
-   const revealed = revealLetters.join(" ");
-   wordInProgress.innerText = revealed;
-
+   }
+   //-//const revealed = revealLetters.join(" ");
+   //-//wordInProgress.innerText = revealed;
+   /*/refactored/*/
+   wordInProgress.innerText = revealLetters.join(" ");
 }
 placeHolder(word);
+
+
+guessBtn.addEventListener("click", function(e){
+   e.preventDefault();
+   const guess = inputLetter.value;
+   console.log(guess);
+   inputLetter.value = ""
+}
+
+);
