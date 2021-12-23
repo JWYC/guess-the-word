8a307c183de1ef3521a87ff1077830fe0f051ad0
jwyc <janis.wy.chan@gmail.com>
2021-12-22 22:20:36 -0800

    check input for letter, length or blank

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ const placeHolder = function(word){
 //test// console.log(revealLetters);
 //test// console.log(wordArray.length);
    
-//*-*// a forEach is overkill, a "for in" looks at array values only, no need for index in this function //
+//*-*// a forEach is overkill, a "for of" looks at array values only, no need for index in this function //
    //-//wordArray.forEach (function(letter, index){
      //-// revealLetters.push("●");
       //test//console.log(`${letter} ${index}`)
@@ -39,7 +39,7 @@ const placeHolder = function(word){
    }
    //-//const revealed = revealLetters.join(" ");
    //-//wordInProgress.innerText = revealed;
-   /*/refactored/*/
+/*/refactored/*/
    wordInProgress.innerText = revealLetters.join(" ");
 }
 placeHolder(word);
@@ -48,8 +48,29 @@ placeHolder(word);
 guessBtn.addEventListener("click", function(e){
    e.preventDefault();
    const guess = inputLetter.value;
-   console.log(guess);
+   //console.log(guess);
+   
    inputLetter.value = ""
-}
+   
+   response.innerText = ""
+   
+   checkInput(guess);
+   console.log(guess);
+});
 
-);
+const checkInput = function(input){
+   const acceptedLetter = /[a-zA-Z]/;
+   //-//console.log(input.length);
+ if (input == ""){
+    console.log("oops, please enter a letter to guess.");
+ }else if (input.length > 1){
+    console.log("Please, enter only one letter at a time.");
+ } else if (input != input.match(acceptedLetter)){
+    console.log("Sorry only letters are accepted");
+ } else{
+    console.log(`Your guess: ${input}`);
+    return input; 
+ }
+ 
+};
+
